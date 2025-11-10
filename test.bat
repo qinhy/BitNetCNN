d9b3fdba62c688356be2683ca098f66a8675b419
koki.chin <qinhunagyi@gmail.com>
2025-11-10 10:09:42 +0900

    Update test.bat

--- test.bat
+++ test.bat
@@ -1,5 +1,5 @@
 uv run BitYOLOv8.py --model-size medium --epochs 1
 uv run BitConvNeXtv2.py --model-size nano --epochs 1
+uv run BitMobileNetV2.py --epochs 1
 uv run BitResNet.py --model-size 18 --epochs 1
 uv run BitResNet.py --model-size 50 --epochs 1
-uv run BitMobileNetV2.py --epochs 1
