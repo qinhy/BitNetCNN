03f5ed30ca0b6fb2cbb76e3affa2c8e113103b6a
koki.chin <qinhunagyi@gmail.com>
2025-11-26 17:36:04 +0900

    Update linear.py

--- bitlayers/linear.py
+++ bitlayers/linear.py
@@ -1,21 +1,46 @@
 from __future__ import annotations
 
 import json
-import math
-from typing import Optional, Sequence, Tuple, Union
+from typing import Union
 
 from pydantic import BaseModel
 from torch import nn
 import torch
-import torch.nn.functional as F
+
+from .acts import ActModels
+from .bit import Bit
+from .norms import NormModels
+
 
 class LinearModels:
     class BasicModel(BaseModel):
+        bit: bool = True
+        scale_op: str = "median"
+
         def build(self):
             mod = LinearModules
             return mod.__dict__[f'{self.__class__.__name__}'](self)
 
-    type = Union[]
+    class Linear(BasicModel):
+        in_features: int
+        out_features: int
+        bias: bool = True
+
+        bit: bool = True
+        scale_op: str = "median"
+
+    class LinearBn(Linear):
+        bn: NormModels.type
+
+    class LinearAct(Linear):
+        act: ActModels.type
+
+    class LinearBnAct(Linear):
+        bn: NormModels.type
+        act: ActModels.type
+
+    type = Union[Linear,LinearBn,LinearAct,LinearBnAct]
+
 
 class LinearModules:
     class Module(nn.Module):
@@ -24,3 +49,71 @@ class LinearModules:
                 para = para_cls(**para)
             self.para = json.loads(para.model_dump_json())
             super().__init__()
+
+        @torch.no_grad()
+        def to_ternary(self,mods=[]):
+            for m in mods:
+                if self.__dict__[m] and hasattr(self.__dict__[m],'to_ternary'):
+                    setattr(self,m,self.__dict__[m].to_ternary())
+            return self
+
+    class Linear(Module):
+        def __init__(self, para: LinearModels.Linear, para_cls=LinearModels.Linear):
+            super().__init__(para, para_cls)
+            self.para: LinearModels.Linear = self.para
+
+            if para.bit:
+                self.linear = Bit.Linear(
+                    para.in_features, para.out_features,
+                    bias=para.bias, scale_op=para.scale_op,
+                )
+            else:
+                self.linear = nn.Linear(
+                    para.in_features, para.out_features,
+                    para.bias,
+                )
+
+        def forward(self,x):
+            return self.linear(x)
+
+        @torch.no_grad()
+        def to_ternary(self):
+            if hasattr(self.linear,'to_ternary'):return self.linear.to_ternary()
+            print('to_ternary is no support!')
+
+    class LinearBn(Linear):
+        def __init__(self, para: LinearModels.LinearBn, para_cls=LinearModels.LinearBn):
+            super().__init__(para, para_cls)
+            self.bn = para.bn.build()
+
+        def forward(self, x):
+            return self.bn(super().forward(x))
+
+        @torch.no_grad()
+        def to_ternary(self):
+            return nn.Sequential(super().to_ternary(),self.bn)
+
+    class LinearAct(Linear):
+        def __init__(self, para: LinearModels.LinearAct, para_cls=LinearModels.LinearAct):
+            super().__init__(para, para_cls)
+            self.act = para.act.build()
+
+        def forward(self, x):
+            return self.act(super().forward(x))
+
+        @torch.no_grad()
+        def to_ternary(self):
+            return nn.Sequential(super().to_ternary(),self.act)
+
+    class LinearBnAct(Linear):
+        def __init__(self, para: LinearModels.LinearBnAct, para_cls=LinearModels.LinearBnAct):
+            super().__init__(para, para_cls)
+            self.bn = para.bn.build()
+            self.act = para.act.build()
+
+        def forward(self, x):
+            return self.act(self.bn(super().forward(x)))
+
+        @torch.no_grad()
+        def to_ternary(self):
+            return nn.Sequential(super().to_ternary(),self.bn,self.act)
